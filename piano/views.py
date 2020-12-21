32e47c6a9fcb7b03ec3f3c67dacaf7c9521271e7
hwoo-c <chrishw.acorn@gmail.com>
2020-12-21 19:19:22 +0900

    Render index.html in index view

--- piano/views.py
+++ piano/views.py
@@ -1,7 +1,10 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request):
-    return
-    
+    return render(request, 'piano/index.html')
